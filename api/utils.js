fe2d0902e74eb7f18f56bda4a18ee36960b3495e
nicholascarlin <54487246+nicholascarlin@users.noreply.github.com>
2023-01-18 18:25:52 -0600

    Things and stuff

--- api/utils.js
+++ api/utils.js
@@ -1,18 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
-const jwt = require("jsonwebtoken");
-const token = process.env.JWT_SECRET;
-const fetch = require("node-fetch");
-const OpenAI = require("openai-api");
+const jwt = require('jsonwebtoken');
+const OpenAI = require('openai-api');
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const OPENAI_API_KEY = process.env.OPEN_AI_KEY;
-	//
+//
 
-	const openai = new OpenAI(OPENAI_API_KEY);
+const openai = new OpenAI(OPENAI_API_KEY);
 //TODO: use different secret
 
 /**
@@ -22,49 +20,47 @@ const OPENAI_API_KEY = process.env.OPEN_AI_KEY;
  * 			If the verification passes then we return a user object of info, otherwise we return a token error
  */
 const validateJWT = (auth, res) => {
-	
-	let ub = jwt.verify(
-		auth,
-		process.env.JWT_TOKEN,
-		(err, user) => {
-			if (err) {
-				console.log(err);
-				res.status(400).send({ error: err });
-			}
-			if (!err) {
-			    return user.sub
-			}
+	let ub = jwt.verify(auth, process.env.JWT_TOKEN, (err, user) => {
+		if (err) {
+			console.log(err);
+			res.status(400).send({ error: err });
 		}
-	);
-	console.log(ub)
-    return ub
+		if (!err) {
+			return user.sub;
+		}
+	});
+	console.log(ub);
+	return ub;
 };
-const seeHowManyMade = async(uuid) =>{
-	console.log(uuid)
-	const {data, error} = await supabase.from("li_resumes").select("*").match({"uuid": uuid})
-	if(error){
-		return null
+const seeHowManyMade = async (uuid) => {
+	console.log(uuid);
+	const { data, error } = await supabase
+		.from('li_resumes')
+		.select('*')
+		.match({ uuid: uuid });
+	if (error) {
+		return null;
 	} else {
-		return data.length
+		return data.length;
 	}
-}
+};
 
-const askAI = async(prompt, temperature)=>{
-    let toReturn = await openai.complete({
-        engine: "text-davinci-003",
-        prompt,
-        temperature,
-        max_tokens: 2048,
-        top_p: 1,
-        frequency_penalty: 0.4,
-        presence_penalty: 0,
-    });
-    console.log(toReturn.res)
-    return toReturn.data.choices[0].text;
-}
+const askAI = async (prompt, temperature) => {
+	let toReturn = await openai.complete({
+		engine: 'text-davinci-003',
+		prompt,
+		temperature,
+		max_tokens: 2048,
+		top_p: 1,
+		frequency_penalty: 0.4,
+		presence_penalty: 0,
+	});
+	console.log(toReturn.res);
+	return toReturn.data.choices[0].text;
+};
 
 module.exports = {
 	validateJWT,
 	askAI,
-	seeHowManyMade
+	seeHowManyMade,
 };
