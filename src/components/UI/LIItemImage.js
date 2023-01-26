f37485e6c92d19b4be8cb5f49146bbb3dce17730
nicholascarlin <54487246+nicholascarlin@users.noreply.github.com>
2023-01-25 21:19:39 -0600

    Improvements

--- src/components/UI/LIItemImage.js
+++ src/components/UI/LIItemImage.js
@@ -4,15 +4,19 @@ import { AiOutlineCheck } from 'react-icons/ai';
 
 const LIItemImage = ({ item }) => {
 	const [isItemActive, setItemStatus] = useState(true);
+	const [isShowMoreActive, setShowMoreStatus] = useState(false);
 
 	return (
-		<div
-			onClick={() => {
-				setItemStatus(() => {
-					return !isItemActive;
-				});
-			}}
-			className='w-full'>
+		<div className='w-full relative'>
+			<div
+				onClick={() => {
+					setShowMoreStatus(() => {
+						return !isShowMoreActive;
+					});
+				}}
+				className='absolute bottom-1 right-2 text-xs text-sub-medium cursor-pointer'>
+				{isShowMoreActive ? '(Click Text to Edit) Show Less' : 'Show More'}
+			</div>
 			<div className='w-full flex justify-between items-center'>
 				<div className='flex'>
 					<img
@@ -20,19 +24,28 @@ const LIItemImage = ({ item }) => {
 						src={item.image}
 						alt='Item Icon'
 					/>
-					<div className='flex flex-col py-6 pr-4'>
+					<div className='flex flex-col py-6 pr-4 relative'>
 						<div className='font-medium'>{item.title}</div>
 						<div className='text-sm'>Paylocity - Internship</div>
 						<div className='text-sm text-sub-medium'>
 							{item.start_date} - {item.end_date}
 						</div>
 						<div className='text-sm text-sub-medium'>{item.location || ''}</div>
-						<p contentEditable className='text-sm line-clamp-3'>
+						<p
+							contentEditable={isShowMoreActive}
+							className={`text-sm ${
+								isShowMoreActive ? 'border-primary' : 'line-clamp-3'
+							}`}>
 							{item.description || ''}
 						</p>
 					</div>
 				</div>
 				<div
+					onClick={() => {
+						setItemStatus(() => {
+							return !isItemActive;
+						});
+					}}
 					className={`h-8 w-8 flex-shrink-0 ${
 						isItemActive ? 'border-primary bg-primary' : 'border-sub-medium'
 					} border-2 rounded-full mr-4 transition-all duration-300 flex items-center justify-center`}>
